ccf6acc2667774c598182352e7e87ec913fd36f0
Orlando Junior <orlandojsjunior@hotmail.com>
2024-05-01 14:57:51 -0300

    Exercicio 2.1
    Convertendo expressões para o interpretador

---
+++ Exercicio_2.1.py
@@ -0,0 +1,19 @@
+'''
+Exercício 2.1 Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+10 + 20 x 30
+4² ÷ 30
+(9^{4} + 2) x 6 - 1
+
+'''
+resultado_1 = 10 + 20 * 30
+print(resultado_1)
+
+
+resultado_2 = 4**2 / 3
+print(resultado_2)
+
+
+resultado_3 = (9**4 + 2) * 6 - 1
+print(resultado_3)
+
+
